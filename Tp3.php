dd0a4384c5cffe8526feefc1bc03231d6723079e
Melisa-V-Sapp-L <melisavanesasapp@gmail.com>
2021-09-11 00:13:41 -0300

    'segundo'

---
+++ Tp3.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    
+    <title> Pequeña prueba PhP </title>
+    <link rel="stylesheet" type="text/css" href="portalstyle.php">
+</head>
+<body>
+	<header>
+		<div class="box">
+			<div><img class="logo" src = imagenes/megafono.jpg /></div>
+			<div><h1><?php echo "La voz popular" ?></h1></div>
+        </div> 
+        	
+	</header>
+
+	<nav>
+		<div class="menu">
+				 <div>
+				 	Ultimas noticias. |
+				 </div>
+				 <div>
+				 	|  Ciudad.  |
+    	         </div>
+    	         <div>
+				 	|  Región.  |
+    	         </div>
+    	         <div>
+				 	|  Policiales.  |
+    	         </div>
+    	         <div>
+				 	|  Deportes.  |
+    	         </div>
+    	         <div>
+				 	|  Ocio.  |
+    	         </div>
+    	         <div>
+    	         	|  Videos.
+    	         </div> 
+    	</div>	
+    	<hr>
+	</nav>
+
+	<article>
+		<section class="noticias">
+			<div >
+                 <img class="imagen" src="imagenes/megafono.jpg">
+                 <div>
+                        <a href="ciudad.html">Ver esta noticia </a> 
+                 </div>
+            </div>	
+            <div >
+                 <img class="imagen" src="imagenes/megafono.jpg">
+                 <div>
+                        <a href="region.html">Ver esta noticia </a> 
+                 </div>
+            </div>
+            <div >
+                 <img class="imagen" src="imagenes/megafono.jpg">
+                 <div>
+                        <a href="policiales.html">Ver esta noticia </a> 
+                 </div>
+            </div>
+            <div >
+                 <img class="imagen" src="imagenes/megafono.jpg">
+                 <div>
+                        <a href="deporte.html">Ver esta noticia </a> 
+                 </div>
+            </div>	
+		</section>
+		<section class="redessociales">
+			<div >
+                 <img class="social" src="imagenes/facebook.png">
+                 <div>
+                        
+                 </div>
+            </div>	
+            <div >
+                 <img class="social" src="imagenes/tweeter.png">
+                 <div>
+                        
+                 </div>
+            </div>
+            <div >
+                 <img class="social" src="imagenes/instagram.png">
+                 <div>
+                        
+                 </div>
+            </div>
+            
+		</section>
+	</article>
+
+
+	<footer> 
+		<hr>
+		
+		<div class="box">
+			
+				 <div>
+				 	Diseño web de Melisa V Sapp Ledesma. |
+				 </div>
+				 <div>
+				 	|  Rosario, Argentina.  |
+    	         </div>
+    	         <div>
+    	         	|  &copy 2021
+    	         </div> 
+
+    	</div>	
+    	
+    </footer>
+
+</body>
+</html>
---
+++ portalstyle.php
@@ -0,0 +1,71 @@
+<?php
+
+     header("Content-Type: text/css; charset: UTF-8")
+
+?>
+
+body {
+	background: #00B8D1;
+
+	 }
+
+header{
+	background: #ffee00;
+	margin: 1px 2px 2px 2px;
+	padding: 1px 2px 2px 2px;
+	font-family: "Copperplate";	
+}
+
+nav{
+	background: #ffee00;
+	margin: 1px 2px 2px 2px;
+	padding: 1px 2px 2px 2px;			
+}
+
+article{
+	margin: 1px 2px 2px 2px;
+	display: flex;
+	justify-content: space-between;
+	}	
+
+section{
+	margin: 3px 3px 3px 3px;
+	padding: 3px 3px 3px 3px;
+}
+
+
+h1{
+
+}
+
+.box{
+	display: flex;
+    align-items: stretch;
+}
+
+.menu{
+	display: flex;
+    justify-content: flex-end;
+}
+
+.logo{
+	width: 200px; 
+	height: 200px;
+}
+
+.social{
+    padding: 9px 0px 4px 4px;
+	width: 100px; 
+	height: 100px;
+}
+
+.noticias{
+	
+
+}
+
+.redessociales{
+	background: #000000;
+	width: 120px; 
+	height: 350px;
+}
